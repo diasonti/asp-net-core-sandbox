abbf71b2ebe00daf13762e35e11f8523f706c2f5
Vladimir Danilov <diasonti@gmail.com>
2019-10-06 15:59:40 +0600

    Add sample db data

--- aspNetCoreSandbox/Migrations/PostgresSetup.sql
+++ aspNetCoreSandbox/Migrations/PostgresSetup.sql
@@ -7,3 +7,8 @@ CREATE TABLE todo_item
 	created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
 );
 GRANT ALL ON TABLE todo_item TO aspnet;
+
+-- Sample data
+INSERT INTO todo_item (text, created_at) VALUES ('Do the dishes', '2019-10-03 12:49:32.000000');
+INSERT INTO todo_item (text, created_at) VALUES ('Make the bed', '2019-10-04 12:49:32.000000');
+INSERT INTO todo_item (text, created_at) VALUES ('Do the workout', '2019-10-05 12:44:32.000000');
